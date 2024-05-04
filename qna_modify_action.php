54d0e3db827b31c4279766b03893c6b820648c49
윤혜준 <140903134+mu-pamang@users.noreply.github.com>
2024-05-04 16:45:33 +0900

    qna_modify_action.php

---
+++ qna_modify_action.php
@@ -0,0 +1,25 @@
+<?php
+$connect = mysqli_connect("  ", "  ", "  ", "  ") or die("connect fail");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $number = $_POST['number'];
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+
+    $query = "UPDATE jilmun SET title='$title', content='$content' WHERE number=$number";
+    $result = mysqli_query($connect, $query);
+
+    if ($result) {
+        ?>
+        <script>
+            alert("수정되었습니다.");
+            location.replace("./qna_view.php?number=<?= $number ?>");
+        </script>
+        <?php
+    } else {
+        echo "fail";
+    }
+} else {
+    echo "잘못된 접근입니다.";
+}
+?>
