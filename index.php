3297adeb2f57c2efb2e58533f5d9741a7edc0ced
윤혜준 <140903134+mu-pamang@users.noreply.github.com>
2024-05-04 17:10:33 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/style_index.css">
+    <script src="./js/script.js"></script>
+    <title>
+        Hye Jun's
+    </title>
+</head>
+<body>
+    <header>
+        <div class="header_container">
+            <div class="logo_container">
+                <a href="/index2.php">Hye Jun's Web</a>
+            </div>
+            <div class="nav_container" id="nav_menu">
+                <div class="menu_container">
+                    <ul class="menu">
+                        <li class="menu_1">
+                            <a class="menu_title"; href="/board.php">공지사항</a>
+                            <ul class="menu_1_content">
+                                <li class="menu_title"><a class="menu_title" href="/board.php">공지사항</a></li>
+                            </ul>
+                        </li>
+                        </li>
+                    </ul>
+                </div>
+                <div class="login_container">
+                    <ul class="login">
+                        <li class="menu_login"><a class="menu_title" href="/login.php">로그인</a></li>
+                        <li class="menu_join"><a class="menu_title" href="/join.php">회원가입</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="main_container">
+        <div class="conA">
+            <div class="slide img1"></div>
+            <div class="slide img2"></div>
+            <div class="slide img3"></div>
+        </div>
+            <div class="conB_container">
+                <div class="conB_small_container">
+                    <div class="conB_icon">
+                        <i class="fas fa-code icon"></i>
+                    </div>
+                </div>
+                <div class="conB_small_container">
+                    <div class="conB_icon">
+                        <i class="fas fa-cogs icon"></i>
+                    </div>
+                </div>
+                <div class="conB_small_container">
+                    <div class="conB_icon">
+                        <i class="fas fa-plane icon"></i>
+                    </div>
+            </div>
+        </div>
+    </div>
+    </form>
+    <footer>
+        <div class="footer_container">
+            <div class="footA">          
+            </div>
+            <div class="footB">
+                Copyright © 2023 All Rights Reserved.
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
