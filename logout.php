6407949b7e0ab5cb7be27f169dea9f31b2fb2791
윤혜준 <140903134+mu-pamang@users.noreply.github.com>
2024-05-04 17:00:31 +0900

    logout.php

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php 
+  include 'session.php';
+?>
+<!doctype html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+      if ( $jb_login ) {
+        session_destroy();
+        echo '<h1>로그아웃 하였습니다.</h1>';
+      } else {
+        echo '<h1>로그인 상태가 아닙니다.</h1>';
+      }
+    ?>
+  </body>
+</html>
