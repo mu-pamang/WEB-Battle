2a1e75a48775ffac75f816e60a5d599e074b877f
윤혜준 <140903134+mu-pamang@users.noreply.github.com>
2024-05-04 17:07:35 +0900

    indexuser.php

---
+++ indexuser.php
@@ -0,0 +1,16 @@
+<?php
+                $connect = mysqli_connect('  ', '', '', '') or die ("connect fail");
+                $query ="select * from board order by number desc";
+                $result = $connect->query($query);
+                $total = mysqli_num_rows($result);
+ 
+                session_start();
+ 
+                if(isset($_SESSION['userid'])) {
+                        echo $_SESSION['userid'];?>님 안녕하세요
+                        <br>
+                } else{
+                  <button onclick="location.href='./login.php'">로그인</button>
+                  <br>
+                }
+?>
